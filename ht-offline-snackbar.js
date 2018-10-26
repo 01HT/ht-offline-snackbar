5cd31706e13ea07abe8015c8ebfed87e92ecd63b
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2018-10-26 12:19:47 +0300

    created

---
+++ ht-offline-snackbar.js
@@ -0,0 +1,41 @@
+import { LitElement, html } from "@polymer/lit-element";
+
+class HTOfflineSnackbar extends LitElement {
+  render() {
+    return html`
+      <style>
+      :host {
+        display: block;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: 12px;
+        background-color: #323232;
+        color: #f1f1f1;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        text-align: center;
+        will-change: transform;
+        transform: translate3d(0, 100%, 0);
+        transition-property: visibility, transform;
+        transition-duration: 0.2s;
+        visibility: hidden;
+      }
+      :host(.opened) {
+        visibility: visible;
+        -webkit-transform: translate3d(0, 0, 0);
+        transform: translate3d(0, 0, 0);
+      }
+      @media (min-width: 460px) {
+        :host {
+          width: 320px;
+          margin: auto;
+        }
+      }
+    </style>
+    <slot></slot>
+  `;
+  }
+}
+
+window.customElements.define("ht-offline-snackbar", HTOfflineSnackbar);
